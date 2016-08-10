82f3860035fc25986c95a31cc1c5bcbb4b0ee7bd
Mackenzie, Amy <hicks.264@gmail.com>
2016-08-10 12:00:01 -0700

    jquery battleship board w/ clickable squares

--- battleship.js
+++ battleship.js
@@ -17,4 +17,22 @@ $(document).ready(function(){
     };
     createTable();
 
+
+    // function shoot(x,y){
+    // each time a user clicks a space on the board,
+      $("td").on("click",
+      // the function will add the class to change the color and disable the space.
+      function(){
+          $(this).addClass("shot").off("click");
+          // removes one from the torpedoesLeft
+          torpedoesLeft -= 1;
+          // shows the variable torpedoesLeft on the board
+          $("#torpedoesLeft").text(torpedoesLeft);
+      });
+    // }
+
   });// end of document.ready
+// creating variable to show number of torpedoes left to shoot
+var torpedoesLeft = 25
+
+// 2nd 
