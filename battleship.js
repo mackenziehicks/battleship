843c1ae2d1f31487664a47847608bf62407871c2
Mackenzie, Amy <hicks.264@gmail.com>
2016-08-10 14:37:29 -0700

    gameboard with randomly placed ships in the model array

--- battleship.js
+++ battleship.js
@@ -33,6 +33,35 @@ $(document).ready(function(){
 
   });// end of document.ready
 // creating variable to show number of torpedoes left to shoot
-var torpedoesLeft = 25
+var torpedoesLeft = 25;
+var board = [];
 
-// 2nd 
+
+
+function createBoard() {
+  for (var array = 0; array < 10; array++) {
+    board[array] = [0,0,0,0,0,0,0,0,0,0];
+  }
+}
+createBoard();
+
+
+//purpose: to place 5 ships at a random index in the array
+//signature: nothing --> returns index
+//examples: placeShip --> board[0][0]= 1
+function placeShip() {
+  //sets the number of ships to zero before the function is run
+  var counterOfShips = 0;
+  //if the counterOfShips is less than 5, the computer will pick a random number and defines it as var index
+  while (counterOfShips < 5) {
+    var index1 = Math.floor(Math.random() * (9) + 1);
+    var index2 = Math.floor(Math.random() * (9) + 1);
+    while (board[index1][index2] != 0){
+      index1 = Math.floor(Math.random() * (9) + 1);
+      index2 = Math.floor(Math.random() * (9) + 1);
+    }
+    board[index1][index2] = 1;
+    counterOfShips += 1;
+    console.log(index1,index2);
+  }
+}
