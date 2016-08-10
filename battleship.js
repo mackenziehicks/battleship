6e5556798f29fc2719929fae44e53fb2e6bc70d1
Mackenzie, Amy <hicks.264@gmail.com>
2016-08-10 15:52:21 -0700

    gameboard that changes colors based on if 'ship' is present or not

--- battleship.js
+++ battleship.js
@@ -17,13 +17,22 @@ $(document).ready(function(){
     };
     createTable();
 
-
     // function shoot(x,y){
     // each time a user clicks a space on the board,
       $("td").on("click",
       // the function will add the class to change the color and disable the space.
       function(){
+        // if statement that pulls each digit from the ID and places it into the two indexes of the array and asks if it has a value or not
+        if
+        //.attr selects the attribute(anything after an equals sign) that you specify, pulling it from the location (i.e. using 'this') 
+         (board[($(this).attr("id").substring(0,1))][($(this).attr("id").substring(1))] === 0){
+          // adds the class to change the screen color to show we already have shot at this space and disable the click ability
           $(this).addClass("shot").off("click");
+        }
+        // if the array has a 1 in this spot, add teh class "hit" to change the color to red and disable the click function
+        else{
+          $("#"+($(this).attr("id"))).addClass("hit").off("click");
+        }
           // removes one from the torpedoesLeft
           torpedoesLeft -= 1;
           // shows the variable torpedoesLeft on the board
@@ -35,6 +44,8 @@ $(document).ready(function(){
 // creating variable to show number of torpedoes left to shoot
 var torpedoesLeft = 25;
 var board = [];
+var index1;
+var index2;
 
 
 
@@ -54,8 +65,8 @@ function placeShip() {
   var counterOfShips = 0;
   //if the counterOfShips is less than 5, the computer will pick a random number and defines it as var index
   while (counterOfShips < 5) {
-    var index1 = Math.floor(Math.random() * (9) + 1);
-    var index2 = Math.floor(Math.random() * (9) + 1);
+    index1 = Math.floor(Math.random() * (9) + 1);
+    index2 = Math.floor(Math.random() * (9) + 1);
     while (board[index1][index2] != 0){
       index1 = Math.floor(Math.random() * (9) + 1);
       index2 = Math.floor(Math.random() * (9) + 1);
@@ -63,5 +74,7 @@ function placeShip() {
     board[index1][index2] = 1;
     counterOfShips += 1;
     console.log(index1,index2);
+
   }
 }
+placeShip();
